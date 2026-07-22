0238bacd4cd0fa83777b86e45011700e178b424f
woni <jsw321jsw321@naver.com>
2026-07-22 11:25:59 +0900

    Initial commit: Spring Boot 프로젝트 세팅

---
+++ src/main/java/com/hongmap/hongmapbackend/HongmapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hongmap.hongmapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongmapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongmapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongmap/hongmapbackend/HongmapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongmap.hongmapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongmapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
